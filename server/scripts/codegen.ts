97f1ba3529fe9c5bb773d183dca43fba93cdd049
Jin-Hyeok, Cha <Jin-Hyeok.Cha@outlook.kr>
2023-06-30 21:19:06 +0900

    move codegen to scripts

--- server/scripts/codegen.ts
+++ server/scripts/codegen.ts
@@ -1,12 +1,12 @@
 import fastify, { FastifyInstance } from 'fastify';
 import mercurius from 'mercurius';
 import codegenMercurius, { loadSchemaFiles } from 'mercurius-codegen';
-import { join, resolve } from 'path';
+import { resolve } from 'path';
 
 const server: FastifyInstance = fastify({
   logger: true,
 });
-const { schema } = loadSchemaFiles(join(__dirname, 'graphql', '*.graphql'));
+const { schema } = loadSchemaFiles(resolve(__dirname, '..', 'src', 'graphql', '*.graphql'));
 server.register(mercurius, { schema });
 codegenMercurius(server, {
   codegenConfig: { scalars: { DateTime: 'Date' } },
